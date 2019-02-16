f8e6251c6209f8fa06ec3391c2cb79efe893ee12
Derya Aydede <derya.aydede@gmail.com>
2019-02-15 21:16:30 -0800

    AAAAAAAAAAAAAAAAAAA

---
+++ Assets/Scripts/Internal/App.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class App
+{
+    public const String ROTATE_LEFT_KEY = "a";
+    public const String ROTATE_RIGHT_KEY = "d";
+    public const String ACCELERATE_KEY = "w";
+    public const String DECERATE_KEY = "s";
+    public const String SHOW_MINIMAP_KEY = "v";
+    public const String BRAKE_KEY = "space";
+}
+
---
+++ Assets/Scripts/MAAAAAAAAAIIIIIINNNNNN.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MAAAAAAAAAIIIIIINNNNNN : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void handleControls()
+    {
+        bool holdingLeft = Input.GetKey(App.ROTATE_LEFT_KEY);
+        bool holdingRight = Input.GetKey(App.ROTATE_RIGHT_KEY);
+        bool holdingGas = Input.GetKey(App.ACCELERATE_KEY);
+        bool holdingReverse = Input.GetKey(App.DECERATE_KEY);
+    }
+
+}
