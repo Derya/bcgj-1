d226cb10f7c64f49edc9184dfc4a6c50e3ca7cf8
Derya Aydede <derya.aydede@gmail.com>
2019-02-17 15:39:54 -0800

    intro

---
+++ Assets/Scripts/IntroScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroScript : MonoBehaviour
+{
+    [SerializeField]
+    Texture2D image;
+
+    Rect rect;
+
+    void OnGui()
+    {
+        GUI.DrawTexture(rect, image, ScaleMode.StretchToFill);
+    }
+
+    void Update()
+    {
+        rect = new Rect(0, 0, Screen.width, Screen.height);
+    }
+}
